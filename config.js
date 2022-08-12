4cea628e5a803e531666e9957a885e6615ffa400
singhveerraj <singhveerraj.rv@gmail.com>
2022-08-12 20:09:27 +0530

    Co-authored-by: MahimaAsthana <MahimaAsthana@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDHC-idZN_ezOdjbJwLQBgXF0TYubj51QU",
+    authDomain: "storytelling-e7444.firebaseapp.com",
+    databaseURL: "https://storytelling-e7444-default-rtdb.firebaseio.com",
+    projectId: "storytelling-e7444",
+    storageBucket: "storytelling-e7444.appspot.com",
+    messagingSenderId: "5479172186",
+    appId: "1:5479172186:web:388840633847416c337063"
+  };
